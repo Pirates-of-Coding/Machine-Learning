7b7812d82732a39ee5fe50f9e443ee528a624137
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2020-04-04 17:02:07 +0530

    Update DecisionTree.py

--- Decision-Tree-Regression/DecisionTree.py
+++ Decision-Tree-Regression/DecisionTree.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 #importing dataset 
 dataset = pd.read_csv("Position_Salaries.csv")
-X = dataset.iloc[:,1:2]
+X = dataset.iloc[:,1:2].values
 Y = dataset.iloc[:,2:3]
 print("Dataset Imported")
 
@@ -15,12 +15,24 @@ regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, Y)
 print("Model trained")
 
-#visualising the Decision Tree Regressor
-plt.title("Truth or Bluff(Decision Tree Regression)")
-plt.xlabel("Position Level")
-plt.ylabel("Salary")
-plt.scatter(X,Y, color="red",label="Original Values")
-plt.plot(X,regressor.predict(X), label="Model Prediction")
-print("Graph opened")
+# #visualising the Decision Tree Regressor
+# plt.title("Truth or Bluff(Decision Tree Regression)")
+# plt.xlabel("Position Level")
+# plt.ylabel("Salary")
+# plt.scatter(X,Y, color="red",label="Original Values")
+# plt.plot(X,regressor.predict(X), label="Model Prediction")
+# print("Graph opened")
+# plt.legend(loc=0)
+# plt.show()
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red',label="Original Values")
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue', label="Model Prediction")
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
 plt.legend(loc=0)
+print("Graph opened")
 plt.show()
