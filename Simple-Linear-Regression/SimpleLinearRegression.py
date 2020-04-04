874a35ab704345e893704c37ab0ea3dd57eeb8eb
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2020-04-04 15:44:26 +0530

    Update SimpleLinearRegression.py

--- Simple-Linear-Regression/SimpleLinearRegression.py
+++ Simple-Linear-Regression/SimpleLinearRegression.py
@@ -7,6 +7,7 @@ import pandas as pd
 dataset = pd.read_csv("Salary_Data.csv")
 X = dataset.iloc[:,:-1]  #is same as [:,0]
 Y = dataset.iloc[:,1]
+print("Dataset Imported")
 
 #splitting the data set into training and test
 from sklearn.model_selection import train_test_split
@@ -15,7 +16,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=1/3, random_s
 #fitting Simple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression() 
-regressor.fit(X_train, Y_train)         
+regressor.fit(X_train, Y_train)    
+print("Model Trained")     
 
 #predicting the Test set result
 Y_predict = regressor.predict(X_test)
@@ -27,3 +29,5 @@ plt.ylabel("Years of Experience")
 plt.scatter(X_train,Y_train, color = "red")
 plt.plot(X_train,regressor.predict(X_train), color="blue")
 plt.scatter(X_test,Y_test,color="green")
+print("Showing Graph")
+plt.show()
