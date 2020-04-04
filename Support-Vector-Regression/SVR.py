6c3fe0b1627f4cde2be575ff491b664c2ff71a04
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2020-04-04 15:25:23 +0530

    Add files via upload

---
+++ Support-Vector-Regression/SVR.py
@@ -0,0 +1,30 @@
+#importing libararies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing dataset
+#.values will give the values in an array. (shape: (n,1))
+#.ravel will convert that array shape to (n, )
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1:2].values
+Y = dataset.iloc[:,2:3].values
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+Y = sc_Y.fit_transform(Y)
+
+#fitting SVR to dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = "rbf")
+regressor.fit(X,Y.ravel())
+
+#visualising the SVR results
+plt.title("Truth or Bluff(SVR)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.scatter(X,Y, color="red")
+plt.plot(X,regressor.predict(X))
