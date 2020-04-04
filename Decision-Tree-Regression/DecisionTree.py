729c5796fba64cd3fc63bbc8e986bb2ad85b5127
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2020-04-04 15:26:42 +0530

    Add files via upload

---
+++ Decision-Tree-Regression/DecisionTree.py
@@ -0,0 +1,21 @@
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing dataset 
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1:2]
+Y = dataset.iloc[:,2:3]
+
+#fitting the Decision Tree Regression to dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, Y)
+
+#visualising the Decision Tree Regressor
+plt.title("Truth or Bluff(Decision Tree Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.scatter(X,Y, color="red")
+plt.plot(X,regressor.predict(X))
